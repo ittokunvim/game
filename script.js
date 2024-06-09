616b13cf447db725076c6562667a7e26a6bbb48d
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2024-06-10 01:11:12 +0900

    feat: :tada: Site renewal

--- script.js
+++ script.js
@@ -1,31 +1,78 @@
-/**
- * REQUIRED
- * <div id="games"></div>
- **/
+const games = document.querySelector("#games");
 
-// import games from "./data.json" assert { type: "json" };
+const JsonPath = "./data.json";
+const GamesTitle = "ゲーム一覧";
+
+// JSON structure
+// {
+//   "slug": "string",
+//   "name": "string",
+//   "description": "string",
+//   "thumbnail": "string",
+//   "width": "number",
+//   "height": "number"
+// }
+
+async function fetchJson() {
+	try {
+		const response = await fetch(JsonPath, { cache: "no-store" });
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+window.onload =  async function() {
+	const data = await fetchJson();
+	const gamesTitle = document.createElement("h2");
+	const gamesList = document.createElement("div");
+
+	gamesTitle.textContent = GamesTitle;
+	gamesList.classList.add("list");
+
+	data.forEach((game) => {
+		const myGame = document.createElement('div');
+		const gameThumbnail = createGameThumbnail(game.slug, game.thumbnail);
+		const gameLink = createGameLink(game.slug, game.name);
+		const gameDescription = createGameDescription(game.description);
+		const gameScreenSize = createGameScreenSize(game.width, game.height);
+
+		myGame.classList.add('item');
+
+		myGame.appendChild(gameThumbnail);
+		myGame.appendChild(gameLink);
+		myGame.appendChild(gameDescription);
+		myGame.appendChild(gameScreenSize);
+
+		gamesList.appendChild(myGame);
+	});
+
+	games.appendChild(gamesTitle);
+	games.appendChild(gamesList);
+}
 
 function createGameLink(slug, name) {
-	const createLink = document.createElement('div');
-	createLink.classList.add('link');
+	const myLink = document.createElement('div');
+	myLink.classList.add('link');
 
-	const createAnchor = document.createElement('a');
-	createAnchor.href = slug;
-	createAnchor.textContent = name;
-	createLink.appendChild(createAnchor);
+	const myA = document.createElement('a');
+	myA.href = slug;
+	myA.textContent = name;
+	myLink.appendChild(myA);
 
-	return createLink;
+	return myLink;
 }
 
 function createGameDescription(description) {
-	const createDescription = document.createElement('div');
-	createDescription.classList.add('description');
+	const myDesc = document.createElement('div');
+	myDesc.classList.add('description');
 
-	const createParagraph = document.createElement('p');
-	createParagraph.textContent = description;
-	createDescription.appendChild(createParagraph);
+	const myPara = document.createElement('p');
+	myPara.textContent = description;
+	myDesc.appendChild(myPara);
 
-	return createDescription;
+	return myDesc;
 }
 
 function splitImageSize(image) {
@@ -35,53 +82,29 @@ function splitImageSize(image) {
 }
 
 function createGameThumbnail(slug, image) {
-	const createThumbnail = document.createElement('div');
-	createThumbnail.classList.add('thumbnail');
+	const myThumbnail = document.createElement('div');
+	myThumbnail.classList.add('thumbnail');
 
-	const createImage = document.createElement('img');
+	const myImg = document.createElement('img');
 	const imageUrl = slug + "/" + image;
 	const [width, height] = splitImageSize(image);
 
-	createImage.src = imageUrl;
-	createImage.width = width;
-	createImage.height = height;
-	createImage.loading = "lazy";
-	createThumbnail.appendChild(createImage);
+	myImg.src = imageUrl;
+	myImg.width = width;
+	myImg.height = height;
+	myImg.loading = "lazy";
+	myThumbnail.appendChild(myImg);
 
-	return createThumbnail;
+	return myThumbnail;
 }
 
 function createGameScreenSize(width, height) {
-	const createScreenSize = document.createElement('div');
-	createScreenSize.classList.add('size');
+	const myScreenSize = document.createElement('div');
+	myScreenSize.classList.add('size');
 
-	const createParagraph = document.createElement('p');
-	createParagraph.textContent = "画面サイズ: " + width + "x" + height;
-	createScreenSize.appendChild(createParagraph);
+	const myPara = document.createElement('p');
+	myPara.textContent = "画面サイズ: " + width + "x" + height;
+	myScreenSize.appendChild(myPara);
 
-	return createScreenSize;
-}
-
-const gameList = document.querySelector('#games');
-
-window.onload =  async function() {
-	const response = await fetch('./data.json');
-	const games = await response.json();
-
-	games.forEach(function(game) {
-		const createGame = document.createElement('div');
-		createGame.classList.add('game');
-
-		const gameThumbnail = createGameThumbnail(game.slug, game.thumbnail);
-		const gameLink = createGameLink(game.slug, game.name);
-		const gameDescription = createGameDescription(game.description);
-		const gameScreenSize = createGameScreenSize(game.width, game.height);
-
-		createGame.appendChild(gameThumbnail);
-		createGame.appendChild(gameLink);
-		createGame.appendChild(gameDescription);
-		createGame.appendChild(gameScreenSize);
-
-		gameList.appendChild(createGame);
-	});
+	return myScreenSize;
 }
